bd7f94784cfb37ddee5977d425350eae43fde74d
arthurdotwork <35616365+arthurdotwork@users.noreply.github.com>
2025-04-27 13:48:43 +0200

    feat: reinstall web from scratch

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,49 +1,34 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./main.css";
 
-import * as TanstackQuery from './integrations/tanstack-query/provider.tsx';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen';
+const queryClient = new QueryClient();
 
-import './styles.css';
-import reportWebVitals from './reportWebVitals.ts';
-
-// Create a new router instance
 const router = createRouter({
 	routeTree,
 	context: {
-		...TanstackQuery.getContext(),
+		queryClient: queryClient,
 	},
-	defaultPreload: 'intent',
-	scrollRestoration: true,
-	defaultStructuralSharing: true,
-	defaultPreloadStaleTime: 0,
 });
 
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
 	interface Register {
 		router: typeof router;
 	}
 }
 
-// Render the app
-const rootElement = document.getElementById('app');
-if (rootElement && !rootElement.innerHTML) {
-	const root = ReactDOM.createRoot(rootElement);
-
-	root.render(
+const root = document.getElementById("root");
+if (root) {
+	const rootElement = createRoot(root);
+	rootElement.render(
 		<StrictMode>
-			<TanstackQuery.Provider>
+			<QueryClientProvider client={queryClient}>
 				<RouterProvider router={router} />
-			</TanstackQuery.Provider>
+			</QueryClientProvider>
 		</StrictMode>,
 	);
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
