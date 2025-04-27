ed00450dd0e1dcfe884cfb0bc4eb98bbe23e11aa
Arthur <35616365+arthurdotwork@users.noreply.github.com>
2025-04-27 17:04:41 +0200

    feat: register ui (#6)

---
+++ apps/web/src/routes/(auth)/register.tsx
@@ -0,0 +1,8 @@
+import AuthRegisterForm from "@/components/auth.register.form.tsx";
+import { createFileRoute } from "@tanstack/react-router";
+
+const Page = () => {
+	return <AuthRegisterForm />;
+};
+
+export const Route = createFileRoute("/(auth)/register")({ component: Page });
